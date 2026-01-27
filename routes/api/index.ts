bf41ce0cfa883610f49a5b371558697ea0c53afd
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-01-27 21:29:42 +0800

    style: lint files

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -1,7 +1,7 @@
 import { $ } from 'bun';
 import { defineHandler } from 'nitro/h3';
 
-export default defineHandler(async (event) => {
+export default defineHandler(async () => {
   const welcome = await $`echo "Hello from API!"`.text();
   return { message: welcome };
 });
